2a7475b5c57f7c112e5d157ecea499bdd932c5cb
sandeep82945 <sandeep.kumar82945@gmail.com>
2020-10-03 09:15:27 +0530

    Update change_file.py

--- change_file.py
+++ change_file.py
@@ -1,1 +1,17 @@
 
+from shutil import copyfile
+import os
+
+git_dir = os.path.join("git", "allennlp")
+allen_dir = os.path.join("src", "allennlp")
+
+f = open("filelist.txt")
+file_contents = f.read()
+contents_split = file_contents.splitlines()
+f.close()
+
+for file in contents_split:
+    src = os.path.join(git_dir, file)
+    dest = os.path.join(allen_dir, file)
+    copyfile(src, dest)
+
